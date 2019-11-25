1bcd1bb75dc3cf22d08673c1cd9e6c7bb19bb98e
Thomas Schissler <thomas@agilemax.de>
2019-11-25 18:01:16 +0100

    Updated to newer cucumber version

--- game-controller/src/test/java/bdd/features/steps/ShipValidationSteps.java
+++ game-controller/src/test/java/bdd/features/steps/ShipValidationSteps.java
@@ -1,8 +1,8 @@
 package bdd.features.steps;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 import org.junit.Assert;
 import org.scrum.psd.battleship.controller.GameController;
 import org.scrum.psd.battleship.controller.dto.Letter;
