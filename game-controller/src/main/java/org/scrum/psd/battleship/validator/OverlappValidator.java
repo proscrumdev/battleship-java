ab2288fd1e80855116405fb0767b2312eb86bdbe
eklalei <aleksander.klinkowski@ericsson.com>
2022-05-20 14:31:47 +0200

    Overlapp validator implemented

--- game-controller/src/main/java/org/scrum/psd/battleship/validator/OverlappValidator.java
+++ game-controller/src/main/java/org/scrum/psd/battleship/validator/OverlappValidator.java
@@ -9,8 +9,15 @@ public class OverlappValidator implements FleetValidator {
 
     @Override
     public boolean validate(List<Ship> fleet, Ship ship, Position position) {
-        // TODO Auto-generated method stub
-        return false;
+        for (Ship sh: fleet) {
+            for (Position pos: sh.getPositions()) {
+                if (position.equals(pos)) {
+                    System.out.println("Position is already taken by ship: " + sh.getName());
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
 }
