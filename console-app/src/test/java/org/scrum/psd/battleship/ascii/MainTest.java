757e5bd5ef9f7c27e356b4e91a6cbabf90e985ab
Filip Kucera (cen80113) <fikucera@csas.cz>
2022-10-25 15:09:32 +0200

    6_1 - tests - ships are not crossing and are placed differently for games

--- console-app/src/test/java/org/scrum/psd/battleship/ascii/MainTest.java
+++ console-app/src/test/java/org/scrum/psd/battleship/ascii/MainTest.java
@@ -6,6 +6,12 @@ import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.scrum.psd.battleship.controller.dto.Letter;
 import org.scrum.psd.battleship.controller.dto.Position;
+import org.scrum.psd.battleship.controller.dto.Ship;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Execution(ExecutionMode.CONCURRENT)
 public class MainTest {
@@ -25,5 +31,35 @@ public class MainTest {
         Position actual = Main.parsePosition("B1");
         //then
         Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testInitializeEnemyFleet_notCrossingShips() {
+        Main.InitializeEnemyFleet();
+        List<Ship> enemyFleet = Main.enemyFleet;
+        Assertions.assertEquals(enemyFleet.size(), 5);
+        List<Position> positions = enemyFleet.stream().flatMap(
+                ship -> ship.getPositions().stream()
+        ).collect(Collectors.toList());
+
+        Set<String> stringPositions = positions.stream().map(Position::toString).collect(Collectors.toSet());
+        Assertions.assertEquals(stringPositions.size(), positions.size());
+    }
+
+    @Test
+    public void testInitializeEnemyFleet_gamesDiffer() {
+        String run1 = String.join("", getStringPositions());
+        String run2 = String.join("", getStringPositions());
+        Assertions.assertNotEquals(run1, run2);
+    }
+
+    private Set<String> getStringPositions() {
+        Main.InitializeEnemyFleet();
+        List<Ship> enemyFleet = Main.enemyFleet;
+        List<Position> positions = enemyFleet.stream().flatMap(
+                ship -> ship.getPositions().stream()
+        ).collect(Collectors.toList());
+
+        return positions.stream().map(Position::toString).collect(Collectors.toSet());
     }
 }
